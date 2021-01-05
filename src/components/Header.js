acc84837f3e0a58c9f3aa115230566e5e140254f
Juan Bonami <Juanbonami@gmail.com>
2021-01-04 23:17:52 -0600

    added header with four buttons

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <div>SHOP</div>
+                <div>INSIDE TOKYOBIKE</div>
+                <div>MY ACCOUNT</div>
+                <div>CART</div>
+            </div>
+        )
+    }
+}
