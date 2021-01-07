cb69fa13b5b10aef6efb91c5732f54d8d9f6473f
Juan Bonami <Juanbonami@gmail.com>
2021-01-06 23:25:43 -0600

    setting up image container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
+// import src from '*.avif'
 import React, { Component } from 'react'
+import logo from '../images/logo.png';
+import logo2 from '../images/tokyobike-Oksana.jpg';
 
 export default class Header extends Component {
     render() {
         return (
-            <div className="header">
-                <div>SHOP</div>
-                <div>INSIDE TOKYOBIKE</div>
-                <div>MY ACCOUNT</div>
-                <div>CART</div>
+            <div>
+                <div className="header">
+                    <div className="logo">
+                        <img src={logo} width="100px" height="24px"></img>  
+                    </div>
+                    <div>SHOP</div>
+                    <div>INSIDE TOKYOBIKE</div>
+                    <div>MY ACCOUNT</div>
+                    <div>CART</div>
+                </div>
+                <div className="image-container">
+                  <img src={logo2}></img>  
+                </div>
+                
             </div>
+            
         )
     }
 }
