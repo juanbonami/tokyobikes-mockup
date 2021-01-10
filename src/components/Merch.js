e67c33e850e1dacd96ad272c37c9897b5ee30c4a
Juan Bonami <Juanbonami@gmail.com>
2021-01-10 14:41:19 -0600

    added new component for merchandise

---
+++ src/components/Merch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Merch extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
