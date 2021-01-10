14eb4a458ebd6460f1f8b9e0fbd495c8e632c2fb
Juan Bonami <Juanbonami@gmail.com>
2021-01-10 15:33:01 -0600

    imported images to merch component

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import img1 from '../images/Sport_black.jpg';
+import img2 from '../images/CS26_mustard.jpg';
+import img3 from '../images/Bisou_blue-gray.jpg';
+import img4 from '../images/SS_deep-forest.jpg';
+import img5 from '../images/MiniVelo_rose.jpg';
+import img6 from '../images/Little_cedar-green.jpg';
 
 export default class Merch extends Component {
     render() {
         return (
             <div>
                 <div id="merch-header">MEET THE TOKYOBIKE LINE UP</div>
+                <div>
+                    <img src={img1}></img>
+                    <img></img>
+                    <img></img>
+                    <img></img>
+                    <img></img>
+                    <img></img>
+                </div>
             </div>
         )
     }
